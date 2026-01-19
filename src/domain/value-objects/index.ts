045fb8b2dc3181ae5a0c07007011da189f2a4f81
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-19 22:28:07 +0900

    タイトルを修正

---
+++ src/domain/value-objects/index.ts
@@ -0,0 +1,3 @@
+import { TaskComplete} from "./task_complete"
+import { TaskId } from "./task_id"
+import { TaskTitle } from "./task_title"
--- src/domain/value-objects/task_title.ts
+++ src/domain/value-objects/task_title.ts
@@ -1,7 +1,27 @@
 class TaskTitle {
     private readonly title : string;
 
-    constructor(title : string) {
+    private constructor(title : string) {
         this.title = title
     }
+
+    static create(title:string): TaskTitle {
+        const trimmed = title.trim();
+        if (trimmed.length === 0) {
+            throw new Error('タイトルは一文字以上必要です。')
+        }
+        if (trimmed.length > 100) {
+            throw new Error('タイトルは100文字以下です。')
+        }
+        return new TaskTitle(trimmed);
+    }
+
+    getValue(): string {
+        return this.title;
+    }
+
+    equals(other: TaskTitle): boolean {
+        return this.title === other.title;
+    }
 }
+export {TaskTitle}
