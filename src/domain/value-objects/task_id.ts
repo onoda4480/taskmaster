1455f432d022b4b4af3e90e9040e6ee37380c562
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-19 22:34:57 +0900

    idの修正

--- src/domain/value-objects/task_id.ts
+++ src/domain/value-objects/task_id.ts
@@ -1,7 +1,21 @@
 class TaskId {
     private readonly id : number;
 
-    constructor(id: number) {
+    private constructor(id: number) {
         this.id = id
     }
+
+    static create(id: number): TaskId {
+        if (id <= 0) {
+            throw new Error('idが0以下です')
+        }
+        return new TaskId(id);
+    }
+    getValue(): number {
+        return this.id;
+    }
+    equals(other: TaskId): boolean {
+        return this.id === other.id
+    }
 }
+export {TaskId}
