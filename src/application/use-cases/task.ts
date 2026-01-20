f1050dcce864258bb43a48370582f15af6d7cf71
onoda4480 <mianqiangarutimetto@gmail.com>
2026-01-20 23:11:47 +0900

    修正

--- src/application/use-cases/task.ts
+++ src/application/use-cases/task.ts
@@ -5,14 +5,21 @@ import { promises as fs } from 'fs';
 
 class FileTaskRepository implements TaskRepository {
     async save(task: Task): Promise<void> {
-        await fs.writeFile('./data.json', JSON.stringify(task, null, 2));
-    };
+        try {
+            const data = await fs.readFile('./data.json', 'utf-8');
+            const tasks = JSON.parse(data);
+            tasks.push(task);
+            await fs.writeFile('./data.json', JSON.stringify(tasks, null, 2));
+        } catch {
+            await fs.writeFile('./data.json', JSON.stringify([task], null, 2));
+        }
+    }
     async findById(id: TaskId): Promise<Task | null> {
         const data = await fs.readFile('./data.json', 'utf-8');
         const tasks = JSON.parse(data)
 
-        const task = tasks.find((t: any) => t.id === id);
-        return task || null;
+        const taskData = tasks.find((t: any) => t.id === id.getValue());
+        return taskData || null;
     };
     async findAll(): Promise<Task[]> {
         const data = await fs.readFile('./data.json', 'utf-8');
@@ -26,10 +33,8 @@ class FileTaskRepository implements TaskRepository {
         const index = tasks.findIndex((t: any) => t.id === task.getId().getValue());
 
         if (index !== -1) {
-        // そのインデックスのタスクを更新
             tasks[index] = task;
         }
-        // ファイルに書き込む
         await fs.writeFile('./data.json', JSON.stringify(tasks, null, 2));
-    };
-}
+    }
+};
